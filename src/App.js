e943e9e497ea31ae963a5b44b12db6741303eeb2
adamskidev <91947446+adamskidev@users.noreply.github.com>
2022-02-10 01:05:52 +0200

    cw1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './App.css';
 
 const App = () => {
+    const [missions, setMissions] = useState([]);
+
+    console.log(missions)
+
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v3/launches/')
+            .then(response => response.json())
+            .then(value => setMissions(value.filter(mission => mission.launch_year !== '2020')))
+    }, [])
     return (
         <div>
-
+            {missions.map(mission => <div key={mission.flight_number} className={'mission'}>
+                <div>
+                    <h2>{mission.mission_name}</h2>
+                    <span>{mission.launch_year}</span>
+                </div>
+                <img src={mission.links.mission_patch_small} alt={mission.mission_name}/>
+            </div> )}
         </div>
     );
 };
